7108e43bc4b5decaa20eb8ac9b5b5f1f613ea5d3
deryabuder <904921115@qq.com>
2018-07-17 22:53:50 +0800

    icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,138 @@
+<template>
+<div class="icons">
+  <swiper>
+    <swiper-slide v-for='(page, index) of pages' :key='index'>
+    <div class='icon' v-for='item of page' :key='item.id'>
+      <div class='icon-img'>
+        <img class='icon-img-content' :src='item.imgUrl'/>
+      </div>
+      <p class='icon-desc'>{{item.desc}}</p>
+    </div>
+    </swiper-slide>
+  </swiper>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: 'hjk'
+      },
+      {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+        desc: ''
+      },
+      {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/6a/45f595250c73d102.png',
+        desc: ''
+      },
+      {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/20/831d62d2e1c7be02.png',
+        desc: ''
+      },
+      {
+        id: '0005',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png',
+        desc: ''
+      },
+      {
+        id: '0006',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/6c/9e54a8540fee0102.png',
+        desc: ''
+      },
+      {
+        id: '0007',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+        desc: ''
+      },
+      {
+        id: '0008',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/54/35899492b1302802.png',
+        desc: ''
+      },
+      {
+        id: '0009',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/8246f27355943202.png',
+        desc: ''
+      }, {
+        id: '0010',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/80/416c6ab3368d1f02.png',
+        desc: ''
+      }
+      ]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+@import '~styles/mixins.styl';
+
+.icons {
+  height: 0;
+  padding-bottom: 50%;
+}
+
+.icons {
+  margin-top: 0.1rem;
+
+  .icon {
+    position: relative;
+    overflow: hidden;
+    float: left;
+    width: 25%;
+    height: 0;
+    padding-bottom: 25%;
+
+    .icon-img {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0.44rem;
+      box-sizing: border-box;
+      padding: 0.1rem;
+
+      .icon-img-content {
+        display: block;
+        margin: 0 auto;
+        height: 100%;
+      }
+    }
+
+    .icon-desc {
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      height: 0.44rem;
+      line-height: 0.44rem;
+      text-align: center;
+      color: $darkTextColor;
+      ellipsis();
+    }
+  }
+}
+</style>
